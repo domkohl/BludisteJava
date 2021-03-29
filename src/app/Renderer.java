2c8e1da45a209f95a86c0336653f3b2702d64bfd
domkohl <imdomkohl@gmail.com>
2021-03-29 16:30:06 +0200

    pokus o kameru

--- src/app/Renderer.java
+++ src/app/Renderer.java
@@ -4,7 +4,10 @@ import utils.AbstractRenderer;
 import org.lwjgl.glfw.GLFWCursorPosCallback;
 import org.lwjgl.glfw.GLFWScrollCallback;
 import org.lwjgl.glfw.GLFWWindowSizeCallback;
+import utils.GLCamera;
 
+import static utils.GluUtils.gluPerspective;
+import static utils.GluUtils.gluLookAt;
 import static org.lwjgl.opengl.GL11.*;
 
 /**
@@ -15,6 +18,7 @@ import static org.lwjgl.opengl.GL11.*;
  * @since 2020-01-20
  */
 public class Renderer extends AbstractRenderer {
+    private GLCamera camera;
 
     public Renderer() {
         super();
@@ -36,7 +40,7 @@ public class Renderer extends AbstractRenderer {
         glfwCursorPosCallback = new GLFWCursorPosCallback() {
             @Override
             public void invoke(long window, double x, double y) {
-                System.out.println("glfwCursorPosCallback");
+                System.out.println("glfwCursorPosCallback  "+x+"   "+y);
             }
         };
 
@@ -51,22 +55,52 @@ public class Renderer extends AbstractRenderer {
     @Override
     public void init() {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        camera = new GLCamera();
     }
 
     @Override
     public void display() {
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+        //zapnuti Z-bufferu
+        glEnable(GL_DEPTH_TEST);
+
+
+        //Mdoelovaci
+        glMatrixMode(GL_MODELVIEW);
+//        glLoadIdentity();
+//        gluLookAt(0, 0, -0.5, 0, 0, 0, 0, 0, -500.5);
+        glLoadIdentity();
+        camera.setFirstPerson(false);
+        camera.setRadius(20);
+        camera.setMatrix();
+
+
+        //projekce
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(45, width / (float) height, 0.1f, 200.0f);
+
+
+
+
 
         // Rendering triangle by fixed pipeline
         glBegin(GL_TRIANGLES);
         glColor3f(1f, 0f, 0f);
-        glVertex2f(-1f, -1);
+        glVertex3f(-1f, -1,0);
         glColor3f(0f, 1f, 0f);
-        glVertex2f(1, 0);
+        glVertex3f(1, 0,0f);
         glColor3f(0f, 0f, 1f);
-        glVertex2f(0, 1);
+        glVertex3f(0, 1,0);
+
         glEnd();
+
+
+
+
+
+
     }
 
 }
